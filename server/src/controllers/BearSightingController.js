58690a52e8900bd6b30b7ee1fad8a00da6aafb71
Jamal Barnes <mavjbarnes@gmail.com>
2018-08-29 11:19:17 -0700

    sets up bear sight find query values

--- server/src/controllers/BearSightingController.js
+++ server/src/controllers/BearSightingController.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+const _ = require('lodash')
 const BearSighting = require('../models/BearSighting')
 
 exports.createSighting = (req, res, next) => {
@@ -17,15 +19,25 @@ exports.getSightingById = (req, res, next) => {
 }
 
 exports.getSightings = (req, res, next) => {
-  console.log(req.query)
   const sortValue = parseInt(req.query.sort) === 1 || parseInt(req.query.sort) === -1
     ? { num_bears: req.query.sort }
     : { created: 1 }
+  const startDate = req.query.start_date
+    ? moment(req.query.start_date).toDate()
+    : null
+  const endDate = req.query.end_date
+    ? moment(req.query.end_date).toDate()
+    : null
+  const dateValues = { $gte: startDate, $lte: endDate }
+  const dateBetweenValues = _.pickBy(dateValues, _.identity)
+  const values = {
+    created: dateBetweenValues,
+    bear_type: { $regex: req.query.bear_type || '', $options : 'i' },
+    zip_code: { $regex: req.query.zip_code || '', $options : 'i' }
+  }
+  const queryValues = _.omitBy(values, _.isEmpty)
 
-  BearSighting.find({
-    bear_type: { '$regex' : req.query.bear_type || '', '$options' : 'i' },
-    zip_code: { '$regex' : req.query.zip_code || '', '$options' : 'i' }
-  })
+  BearSighting.find(queryValues)
     .sort(sortValue)
     .then(docs => res.json(docs))
     .catch(error => next(error))
